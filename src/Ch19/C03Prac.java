58914d18aa52530950ccfd98cef97ffe30bdfcaf
Anministar <ldh1081703@gmail.com>
2024-08-22 16:08:10 +0900

    second commit

--- src/Ch19/C03Prac.java
+++ src/Ch19/C03Prac.java
@@ -8,11 +8,36 @@ package Ch19;
 // Pencil : write()
 
 // Sharpen, Pencil 객체를 만들때마다 재고가 하나씩 줄어야한다.
+class Sharpen {
+	String model;
+	int year;
+	static int price;
+	
+	public Sharpen(String model, int year) {
+		this.model = model;
+		this.year = year;
+	}
+	
+	
+	public static void write() {
+		System.out.println("가격이 " + price + "인 샤프로 씁니다.");
+		
+	}
+	
+	public void erase() {
+		System.out.println(model + "샤프의 지우개로 지웁니다.");
+	}	
+}
+
 
 public class C03Prac {
 	
 	public static void main(String[] args) {
 		Sharpen item1 = new Sharpen();
+		
+		
+		
+		
 		Pencil item2 = new Pencil();
 		Writing(item1); // "샤프로 씁니다" 가 출력
 		Writing(item2);	// "연필로 씁니다" 가 출력
